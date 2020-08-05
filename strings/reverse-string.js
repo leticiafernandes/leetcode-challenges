559e46d7c5c2dffa1e59d87d0cf6ae9ade9b9443
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-08-05 18:57:27 -0300

    string problem solved

---
+++ strings/reverse-string.js
@@ -0,0 +1,44 @@
+/*
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+--- 
+
+result:
+
+Runtime: 1044 ms, faster than 5.36% of JavaScript online submissions for Reverse String.
+Memory Usage: 46.1 MB, less than 5.17% of JavaScript online submissions for Reverse String.
+
+---
+
+this one i solved quicly but im not really satisfied with the result...
+
+:(
+
+*/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for(i=s.length-1; i >= 0; i--){    
+      s.push(s[i]);
+      s.splice(i,1);
+    }
+};
